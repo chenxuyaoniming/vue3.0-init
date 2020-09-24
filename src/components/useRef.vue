dbdf713667aee1fb0e0839015414c50a54b91c7c
gaochaofeng <v_gaochaofeng01@baidu.com>
2020-09-24 11:00:49 +0800

    feat: useRef

---
+++ src/components/useRef.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button @click="countAdd">
+            {{count}}
+        </button>
+    </div>
+</template>
+
+<script>
+import {setup, ref} from 'vue';
+export default {
+    setup(props, context) {
+        let count = ref(0);
+        console.log(count, 'ref-count')
+        function countAdd() {
+            count.value ++;
+        };
+        return {
+            count,
+            countAdd
+        }
+    }
+}
+</script>
